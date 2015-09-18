c473d3326604af00ce9c3f848def980ea34d79e7
Steven Soloff <s_soloff@yahoo.com>
2015-09-18 14:31:09 -0400

    Use pegs from function parameter rather than referencing globals.

--- imperative/play_tower_of_hanoi.py
+++ imperative/play_tower_of_hanoi.py
@@ -7,14 +7,13 @@ from tower_of_hanoi import Game
 def print_peg(peg):
     print('{0}: {1}'.format(peg.name(), ' - '.join([str(disk.size()) for disk in peg.disks()])))
 
-def print_pegs(*args):
+def print_pegs(pegs):
     print('==========')
-    print_peg(peg_a)
-    print_peg(peg_b)
-    print_peg(peg_c)
+    for peg in sorted(pegs, key=lambda peg: peg.name()):
+        print_peg(peg)
 
 if len(sys.argv) != 2:
-    print('usage: {0} <disk_count>'.format(sys.argv[0]))
+    print('Usage: {0} <disk_count>'.format(sys.argv[0]))
     sys,exit(1)
 
 disk_count = int(sys.argv[1])
@@ -25,5 +24,5 @@ peg_a = game.create_peg('A', disk_count)
 peg_b = game.create_peg('B')
 peg_c = game.create_peg('C')
 
-print_pegs() # display initial state
+print_pegs([peg_a, peg_b, peg_c]) # display initial state
 game.move(disk_count, peg_a, peg_c, peg_b, print_pegs)
